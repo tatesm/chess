3a60f3f78e68a5fa72fbc39cf7d20b25a91025be
Tate Smith <tatems@byu.edu>
2024-09-18 18:20:29 -0600

    Finished ChessMove

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -7,25 +9,42 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessMove {
-    private final starting_pos = ChessPiece.get;
-    private final int col;
+    private final ChessPosition start_pos;
+    private final ChessPosition end_pos;
+    private final ChessPiece.PieceType promotion_piece;
 
     public ChessMove(ChessPosition startPosition, ChessPosition endPosition,
                      ChessPiece.PieceType promotionPiece) {
+        this.start_pos = startPosition;
+        this.end_pos = endPosition;
+        this.promotion_piece = promotionPiece;
     }
 
     /**
      * @return ChessPosition of starting location
      */
     public ChessPosition getStartPosition() {
-        start_pos = ChessPosition.get
+        return start_pos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(start_pos, chessMove.start_pos) && Objects.equals(end_pos, chessMove.end_pos) && promotion_piece == chessMove.promotion_piece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(start_pos, end_pos, promotion_piece);
     }
 
     /**
      * @return ChessPosition of ending location
      */
     public ChessPosition getEndPosition() {
-
+        return end_pos;
     }
 
     /**
@@ -35,6 +54,6 @@ public class ChessMove {
      * @return Type of piece to promote a pawn to, or null if no promotion
      */
     public ChessPiece.PieceType getPromotionPiece() {
-
+        return promotion_piece;
     }
 }
