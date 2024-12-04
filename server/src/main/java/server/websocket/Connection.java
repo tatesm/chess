ddbacc12580a5eabc190a6a00258e5be0b2e75b4
Tate Smith <tatems@byu.edu>
2024-12-03 19:27:19 -0700

    phase 6 3

---
+++ server/src/main/java/server/websocket/Connection.java
@@ -0,0 +1,27 @@
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+
+import java.io.IOException;
+
+public class Connection {
+    private final String playerName; // Player's name or identifier
+    private final Session session;
+
+    public Connection(String playerName, Session session) {
+        this.playerName = playerName;
+        this.session = session;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public boolean isOpen() {
+        return session.isOpen();
+    }
+
+    public void send(String message) throws IOException {
+        session.getRemote().sendString(message);
+    }
+}
---
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -0,0 +1,40 @@
+package server.websocket;
+
+import webSocketMessages.Notification;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    private final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String playerName, Session session) {
+        var connection = new Connection(playerName, session);
+        connections.put(playerName, connection);
+    }
+
+    public void remove(String playerName) {
+        connections.remove(playerName);
+    }
+
+    public void broadcast(String excludePlayer, Notification notification) throws IOException {
+        var toRemove = new ArrayList<Connection>();
+        for (var connection : connections.values()) {
+            if (connection.isOpen() && !connection.getPlayerName().equals(excludePlayer)) {
+                connection.send(notification.toString());
+            } else if (!connection.isOpen()) {
+                toRemove.add(connection);
+            }
+        }
+
+        // Remove closed connections
+        for (var connection : toRemove) {
+            connections.remove(connection.getPlayerName());
+        }
+    }
+
+    public Connection getConnection(String playerName) {
+        return connections.get(playerName);
+    }
+}
